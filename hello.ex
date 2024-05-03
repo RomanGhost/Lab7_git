eb7ee2c303956d28ecc83c9c305d3a6355b6cfff
Oleg Chaplashkin <ochaplashkin@tarantool.org>
2024-05-03 15:47:40 +0400

    Enhance `hello` function with documentation link
    Added a link to the Elixir documentation for IO.puts/2 for clarity and ease of access.
    
    Close #1

---
+++ hello.ex
@@ -0,0 +1,8 @@
+defmodule Solution do
+  # Выводит строку "Hello, World!" в стандартный вывод.
+  # Подробнее о функции IO.puts можно прочитать здесь:
+  # https://hexdocs.pm/elixir/IO.html#puts/2
+  def hello do
+    IO.puts("Hello, World!")
+  end
+end
